21499092e0ad35705071ba8174e46baa9eeecd61
Gabe Fierro <gtfierro225@gmail.com>
2018-04-16 06:29:22 -0700

    add tagsearch impl basic

--- brick2/tagsearch.py
+++ brick2/tagsearch.py
@@ -23,12 +23,12 @@ def get_tags(tags, docs=_docs):
     """
     Return documents that contain the provided set of tags; set intersection
     """
-    tagdocs = {doc['id']: flatten(doc) for doc in _docs.values()}
+    tagdocs = {doc['id']: flatten(doc) for doc in docs.values()}
     results = {}
     s = set(tags)
     for k, doc in tagdocs.items():
         if s.issubset(doc):
-            results[k] = _docs[k]
+            results[k] = docs[k]
     return results
 
 def get_scoped_tag(kvpairs, docs=_docs):
@@ -40,10 +40,19 @@ def get_scoped_tag(kvpairs, docs=_docs):
     for kid, doc in docs.items():
         include = True
         for key, val in kvpairs.items():
-            if include: include = (val == doc.get(key,'') or val in doc.get(key,[]))
+            if isinstance(val, list) and include:
+                include = all((v in doc.get(key,[]) for v in val))
+            elif include:
+                include = (val == doc.get(key,'') or val in doc.get(key,[]))
         if include:
             results[kid] = doc
     return results
+
+def pull_tags(tags, docs=_docs):
+    ret = []
+    for kid, doc in docs.items():
+        ret.append({k: doc.get(k) for k in tags if k in doc})
+    return ret
 
 def get_scoped_tag_path(kvpairs, docs=_docs):
     """
@@ -51,5 +60,69 @@ def get_scoped_tag_path(kvpairs, docs=_docs):
     """
     pass
 
-#get_tags(['fan'])
+from lark import Lark
+from lark import Transformer
+
+parser = Lark(r"""
+    query: statement (";" statement)*
+    statement: list
+        | dict
+        | extract
+    tag: CNAME
+    taglist: tag [tag*]
+    list: "|" taglist
+    dict: tag ":" taglist
+    extract: ">" taglist
+
+    %import common.CNAME
+    %import common.WS
+    %ignore WS
+
+""",start='query')
+class QueryTransformer(Transformer):
+    def __init__(self):
+        self._use = _docs
+        Transformer.__init__(self)
+
+    def list(self, (l,)):
+        self._use = get_tags(l, docs=self._use)
+        return l
+
+    def taglist(self, t):
+        return list(t)
+
+    def dict(self, items):
+        key = items[0]
+        values = items[1]
+        self._use = get_scoped_tag({key: values}, docs=self._use)
+        return {key: values}
+
+    def statement(self, (s,)):
+        return s
+
+    def extract(self, (tl,)):
+        self._use = pull_tags(tl, docs=self._use)
+        return tl
+
+    def tag(self, (t,)):
+        return t.value
+
+def query(q):
+    t = parser.parse(q)
+    qt=  QueryTransformer()
+    qt.transform(t)
+    return qt._use
+
+def queryp(q):
+    printdoc(query(q))
+
+#print query("| air temperature")
+#
+#print query("substance: air return")
+#
+#print query("""
+#| air temperature
+#substance: air return
+#""")
+
 IPython.embed()
